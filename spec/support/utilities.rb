a37ceb4fc4e365083dcd3b731d9283c06aa8775f
kareypowell <kareyp9@gmail.com>
2013-06-27 23:50:18 -0500

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,18 @@
 include ApplicationHelper
 
-def valid_signin(user)
-	fill_in "Email", 		with: user.email
+# def valid_signin(user)
+# 	fill_in "Email", 		with: user.email
+# 	fill_in "Password", with: user.password
+# 	click_button submit
+# end
+
+def sign_in(user)
+	visit signin_path
+	fill_in "Email", 	with: user.email
 	fill_in "Password", with: user.password
-	click_button submit
+	click_button "Sign in"
+	# Sign in when not using Capybara as well.
+	cookies[:remember_token] = user.remember_token
 end
 
 def create_valid_user
@@ -13,6 +22,14 @@ def create_valid_user
 	fill_in "Confirmation", with: "foobar"
 end
 
+def update_user_info(new_name, new_email)
+	fill_in "Name", 						with: new_name
+	fill_in "Email", 						with: new_email
+	fill_in "Password", 				with: user.password
+	fill_in "Confirm Password", with: user.password
+	click_button "Save changes"
+end
+
 def find_user_by_email(user_email)
 	User.find_by(email: user_email)
 end
@@ -20,5 +37,11 @@ end
 RSpec::Matchers.define :have_error_message do |message|
 	match do |page|
 		expect(page).to have_selector('div.alert.alert-error', text: message)
+	end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+	match do |page|
+		expect(page).to have_selector('div.alert.alert-success', text: message)
 	end
 end
