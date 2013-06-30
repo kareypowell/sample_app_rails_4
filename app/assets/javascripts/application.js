42a062f1fc3cc05d348761bb899c7985630c8d23
kareypowell <kareyp9@gmail.com>
2013-06-29 23:16:02 -0500

    Update JS method to limit character count on content compose

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,24 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+
+// var update_chars_left;
+
+// update_chars_left = function(max_length, target_input, display_element) {
+//   var text_length;
+//   text_length = target_input.value.length;
+//   if (text_length >= max_length) {
+//     target_input.value = target_input.value.substring(0, max_length);
+//     return display_element.html("0");
+//   } else {
+//     return display_element.html(max_length - text_length);
+//   }
+// };
+
+// $(document).ready(function() {
+//   $("#micropost_content").keyup(function() {
+//     return update_chars_left(140, $("#micropost_content")[0], $("#chars_left"));
+//   });
+//   return update_chars_left(140, $("#micropost_content")[0], $("#chars_left"));
+// });
--- app/assets/javascripts/static_pages.js.coffee
+++ app/assets/javascripts/static_pages.js.coffee
@@ -5,14 +5,17 @@
 
 # Count down characters for micropost from 140 indicating to the 
 # user how many more characters are left.
+update_chars_left = undefined
 update_chars_left = (max_length, target_input, display_element) ->
+  text_length = undefined
   text_length = target_input.value.length
   if text_length >= max_length
     target_input.value = target_input.value.substring(0, max_length)
     display_element.html "0"
   else
     display_element.html max_length - text_length
-$ ->
+
+$(document).ready ->
   $("#micropost_content").keyup ->
     update_chars_left 140, $("#micropost_content")[0], $("#chars_left")
 
